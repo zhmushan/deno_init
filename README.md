4a973afba923646fd304a8d7055877088d72df81
zhmushan <zhmushan@qq.com>
2019-01-23 16:37:33 +0800

    test: init

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Deno Init
 
+[![Travis branch](https://img.shields.io/travis/zhmushan/deno_init/master.svg?style=flat-square)](https://travis-ci.org/zhmushan/deno_init)
+
 ```sh
 alias deno_init="deno https://raw.githubusercontent.com/zhmushan/deno_init/master/mod.ts --allow-write --allow-run"
 deno_init my_app
---
+++ test.ts
@@ -0,0 +1,37 @@
+import { run, readFileSync, exit, removeAll } from "deno";
+import { assertEqual } from "https://deno.land/x/testing/mod.ts";
+import * as tmpl from "./tmpl.ts";
+
+const projectName = "test_project";
+
+let hsStatus = null;
+run({ args: ["hs", "-p", "8888"] })
+  .status()
+  .then(status => {
+    hsStatus = status;
+  });
+setTimeout(() => {
+  run({
+    args: [
+      "deno",
+      "http://localhost:8888/mod.ts",
+      "--allow-write",
+      "--allow-run",
+      "--reload",
+      "--recompile",
+      projectName
+    ]
+  })
+    .status()
+    .then(status => {
+      if (status.success) {
+        assertEqual(
+          new TextDecoder().decode(readFileSync(`${projectName}/mod.ts`)),
+          tmpl.modts
+        );
+      }
+      removeAll(projectName).then(() => {
+        exit(hsStatus);
+      });
+    });
+}, 2000);
